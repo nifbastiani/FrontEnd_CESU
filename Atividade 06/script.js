33c5aaecbf42a3a5846bf78b72c57eeccff40709
Nicole Fernandes Bastiani <108945028+nifbastiani@users.noreply.github.com>
2026-03-26 11:32:55 -0300

    Add files via upload

--- Atividade 06/script.js
+++ Atividade 06/script.js
@@ -1,13 +1,12 @@
-const cria = document.getElementById("b");
+        const cria = document.getElementById("b");
         const btn = document.getElementById("btn");
 
         const estados = {
             normal: "b_normal.png",
             bravo: "b_bravo.png",
             morto: "b_morto.png",
-            feliz: "b_feliz.png",
             comendo: "b_comendo.png", 
-            alimentado: "b_alimentado.png"
+            alimentado: "b_feliz.png"
         }
 
         let contador = 0;
@@ -30,5 +29,25 @@ const cria = document.getElementById("b");
                     }
                 }, 1000);
 	}
+
+
+    function alimentar() {
+
+    cria.src = estados.comendo;
+    contador = 0;
+
+    console.log("Comendo");
+
+    if (time_click) clearTimeout(time_click);
+
+    time_click = setTimeout(() => {
+        cria.src = estados.alimentado;
+
+        time_out = setTimeout(() => {
+            cria.src = estados.normal;
+        }, 2000);
+
+    }, 1000);
+    }
 
 	controlador();
