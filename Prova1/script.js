fcaa5716a34a3bf1a2fb1c8fbb69aa5f9e6e9038
Nicole Fernandes Bastiani <108945028+nifbastiani@users.noreply.github.com>
2026-04-27 09:28:52 -0300

    Add files via upload

---
+++ Prova1/script.js
@@ -0,0 +1,30 @@
+function mostrar(){
+    document.getElementById("tab").innerHTML = '<img src="img/Tabela_Jogos.png" style = "width: 100%;"></img>'
+}
+
+function revelar(){
+    document.getElementById("imagemJogador").src = "img/_vinicius_junior.png";
+    
+    let nome = document.getElementById("Nome");
+    nome.innerHTML = "Vinícius José Paixão de Oliveira Júnior";
+    nome.classList.remove("placeholder", "col-6");
+    nome.classList.add("card-text");
+
+    let rank = document.getElementById("Rank");
+    rank.innerHTML = "🏃 Rank: 9,5";
+
+    let data = document.getElementById("Data_Nas");
+    data.innerHTML = "📅 Data de Nascimento: 12/07/2000 (25 anos)";
+    data.classList.remove("placeholder", "col-4");
+    data.classList.add("card-text");
+
+    let altura = document.getElementById("Altura");
+    altura.innerHTML = "📏 Altura: 1,76 m";
+    altura.classList.remove("placeholder", "col-4");
+    altura.classList.add("card-text");
+
+    let posicao = document.getElementById("Posicao");
+    posicao.innerHTML = "🏃 Posição: Ponta-esquerda / Atacante";
+    posicao.classList.remove("placeholder", "col-6");
+    posicao.classList.add("card-text");
+}
