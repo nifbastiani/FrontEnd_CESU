e4c59e9e64700bc326d78a75a816ce5f5fc80736
Nicole Fernandes Bastiani <108945028+nifbastiani@users.noreply.github.com>
2026-05-07 11:38:06 -0300

    Update script.js

--- Atividade 08/script.js
+++ Atividade 08/script.js
@@ -6,6 +6,32 @@ function addTask(){
   if(textInput !== ''){
     const newTask = document.createElement('li');
     newTask.innerHTML = `<span>${textInput}</span>`;
+    
+    newTask.innerHTML =
+      `<span>${textInput}</span>
+       <button onclick="editar(this)">Editar</button>
+       <button onclick="remover(this)">Remover</button>
+       <button onclick="concluir(this)">Concluir</button>
+      `
     taskList.appendChild(newTask);
+  }
+}
+
+function remover(button){
+  const taskToRemove = button.parentElement;
+  taskList.removeChild(taskToRemove);
+}
+
+function editar(button){
+  const taskToEdit = button.parentElement;
+  
+}
+
+function concluir(button){
+  const taskToConclude = button.parentElement;
+  if (taskToConclude.classList.toggle('completed')){
+    button.innnerHTML = `Desmarcar`;
+  }else{
+    button.innerHTML = `Concluir`;
   }
 }
