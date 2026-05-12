27fd42d3887d8498c7bb4cabafc0eb5de0600ab7
Nicole Fernandes Bastiani <108945028+nifbastiani@users.noreply.github.com>
2026-05-12 20:06:19 -0300

    Implement task editing feature
    Added editing functionality to tasks with prompt input.

--- Atividade 08/script.js
+++ Atividade 08/script.js
@@ -5,7 +5,6 @@ function addTask(){
   const textInput = taskInput.value.trim()
   if(textInput !== ''){
     const newTask = document.createElement('li');
-    newTask.innerHTML = `<span>${textInput}</span>`;
     
     newTask.innerHTML =
       `<span>${textInput}</span>
@@ -24,11 +23,18 @@ function remover(button){
 
 function editar(button){
   const taskToEdit = button.parentElement;
-  
+  const taskText = taskToEdit.querySelector('span'); 
+
+  const newText = prompt('Edite sua tarefa...', taskText.textContent); 
+  taskText.textContent = newText;
+  //diferença entre innerHTML e textContent, inner lê não só o texto mas os elementos, ex: <span>oi</span>, textContent pega apenas o texto, ex: oi.
+  //o prompt é parecido com aqueles métodos de alert e confirm, abre um campo de digitação para depois dar uma confirmação
+  //então foi criada uma nova var para receber o novo texto, desse campo (prompt), o conteudo antigo da var taskText recebe o newText.
 }
 
 function concluir(button){
   const taskToConclude = button.parentElement;
+  
   if (taskToConclude.classList.toggle('completed')){
     button.innerHTML = `Desmarcar`;
   }else{
