ddfc6923c7f9bd86ae1efdb4d58c2e0667c189a6
Nicole Fernandes Bastiani <108945028+nifbastiani@users.noreply.github.com>
2026-05-14 11:25:55 -0300

    Update script.js

--- Atividade 09/script.js
+++ Atividade 09/script.js
@@ -14,4 +14,29 @@ function coverToCelsius() {
   document.getElementById("tempCelsius").value = tempCelsius;
 }
 
-const media = document.getElementById("media");
+document.getElementById("media").addEventListener("click", calcularMedia);
+
+function calcularMedia(){
+  const n1 = parseFloat(document.getElementById("nota1").value);
+  const n2 = parseFloat(document.getElementById("nota2").value);
+  const n3 = parseFloat(document.getElementById("nota3").value);
+
+  const media = (n1 + n2 + n3)/3;
+
+  const resultado = document.getElementById("resultado");
+  let situacao;
+
+  if(media >= 7 && media <=10){
+      situacao = "APROVADO";
+      resultado.classList.add("aprovado");
+  }else if(media <= 6.9 && media >= 4){
+      situacao = `EXAME - Falta ${10 - media} pontos`;
+      resultado.classList.add("exame");
+  }else{
+      situacao = "REPROVADO";
+      resultado.classList.add("reprovado");
+  }
+
+  resultado.innerHTML = `<span>${situacao}<br>${media.toFixed(2)}</span>`;
+
+}
